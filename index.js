775a23f0b62e4584668a425de2c7a9f74b6b7f5a
DeeBest <simphiwedladla8@gmail.com>
2024-04-26 10:03:35 +0200

    added mouseOver event listener to activate the ripple effect when mouse is hovering over the button

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const footerParagraph = document.getElementById('footer-paragraph');
 const currentDate = new Date().getFullYear();
 footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
+
+const button = document.getElementById('button');
+
+button.addEventListener('mouseover', (event) => {
+  const xPosition = event.pageX - button.offsetLeft;
+  const yPosition = event.pageY - button.offsetTop;
+
+  button.style.setProperty('--xPos', xPosition + 'px');
+  button.style.setProperty('--yPos', yPosition + 'px');
+});
